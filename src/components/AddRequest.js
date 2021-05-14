1bbf1ad10f400d1c44b5d9f2959ed09fccb57925
Shivam Shekhar <shvam0000@gmail.com>
2021-05-14 21:33:02 +0530

    ✨ added more entries in request form

--- src/components/AddRequest.js
+++ src/components/AddRequest.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../assets/main.css'
+
 const AddRequest = () => {
   return (
     <div className="flex flex-col justify-items-center items-center mt-20">
@@ -8,34 +10,70 @@ const AddRequest = () => {
       </div>
       <div className="bg-white w-96 shadow-2xl rounded p-5">
         <form className="space-y-5 mt-5">
+          <label className="mt-5">Name of the patient</label>
           <input
             type="text"
-            placeholder="Name"
+            placeholder="Enter Name of the Patient"
             className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
           />
-          <input
-            type="email"
-            placeholder="Email"
-            className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
-          />
+          <label className="mt-10">Username of the Person Contacting us</label>
+
           <input
             type="text"
-            placeholder="Phone number"
+            placeholder="Enter Username of the Person Contacting us"
             className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
           />
+          <label className="mt-5">Age</label>
           <input
-            type="text"
-            placeholder="Requirement"
+            type="number"
+            placeholder="Enter Age"
             className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
           />
+          <label className="mt-5">Gender</label>
+          <select className="border border-gray-800 w-full h-12 px-3 focus:outline-none">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="others">Others</option>
+          </select>  
+          <label className="mt-">Location</label>
           <input
             type="text"
-            placeholder="City"
+            placeholder="Enter Location"
             className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
           />
+          <label className="mt-5">Oxygen (SPO2) Level</label>
           <input
             type="text"
-            placeholder="Pincode"
+            placeholder="Enter Oxygen (SPO2) Level"
+            className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
+          />
+          <label className="mt-5">Comorbidities</label>
+          <input
+            type="text"
+            placeholder="Enter Comorbidities"
+            className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
+          />
+          <label className="mt-5">Physical Discomfort</label>
+          <input
+            type="text"
+            placeholder="Enter Physical Discomfort"
+            className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
+          />
+          <label className="mt-5">What is this request for?</label>
+          <input
+            type="text"
+            placeholder="Enter What is this request for?"
+            className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
+          />
+          <label className="mt-5">Is transport available?</label>
+          <select className="border border-gray-800 w-full h-12 px-3 focus:outline-none">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+          <label className="mt-5">Anything else you want us to know?</label> 
+          <input
+            type="text"
+            placeholder="Enter Anything else you want us to know?"
             className="border border-gray-800 w-full h-12 px-3 focus:outline-none"
           />
           <button
