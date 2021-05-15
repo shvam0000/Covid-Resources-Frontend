20c2a6aa59da8b8ad31d25513a5c8c0384f6dc4e
Shivam Shekhar <shvam0000@gmail.com>
2021-05-15 20:44:47 +0530

    ⚡️ tailwind config

--- src/components/AddRequest.js
+++ src/components/AddRequest.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import '../assets/main.css'
 
 const AddRequest = () => {
   const stateUT = [
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import '../assets/main.css';
-
 const Login = () => {
   return (
     <React.Fragment>
