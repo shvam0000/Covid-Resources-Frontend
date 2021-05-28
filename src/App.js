82005c8e9deec8d265a7861c0c5a06dfb619afce
Shivam Shekhar <shvam0000@gmail.com>
2021-05-29 00:00:19 +0530

    fixed dispatch error

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUserDetails());
-  }, []);
+  }, [dispatch]);
   return (
     <React.Fragment>
       <Router>
